9851d8952d8b91226f9173c19e0c434557a6bf0b
Raymon Lange <104854233+Raymon-Lange@users.noreply.github.com>
2023-05-16 09:34:22 -0500

    Create level_order.rs
    Given the root of a binary tree, return the level order traversal of its nodes' values. (i.e., from left to right, level by level).
    
     
    
    Example 1:
    
    
    Input: root = [3,9,20,null,null,15,7]
    Output: [[3],[9,20],[15,7]]

---
+++ level_order.rs
@@ -0,0 +1,57 @@
+// Definition for a binary tree node.
+// #[derive(Debug, PartialEq, Eq)]
+// pub struct TreeNode {
+//   pub val: i32,
+//   pub left: Option<Rc<RefCell<TreeNode>>>,
+//   pub right: Option<Rc<RefCell<TreeNode>>>,
+// }
+// 
+// impl TreeNode {
+//   #[inline]
+//   pub fn new(val: i32) -> Self {
+//     TreeNode {
+//       val,
+//       left: None,
+//       right: None
+//     }
+//   }
+// }
+use std::rc::Rc;
+use std::cell::RefCell;
+use std::collections::VecDeque;
+impl Solution {
+    pub fn level_order(root: Option<Rc<RefCell<TreeNode>>>) -> Vec<Vec<i32>> {
+
+    let mut result = Vec::new();
+    let mut queue = VecDeque::new();
+
+    if let Some(root) = root {
+        queue.push_back(root);
+    }
+
+    while !queue.is_empty() {
+        let level_size = queue.len();
+        let mut current_level = Vec::new();
+
+        for _ in 0..level_size {
+            if let Some(node) = queue.pop_front() {
+                let node = node.borrow();
+                current_level.push(node.val);
+
+                if let Some(left) = node.left.clone() {
+                    queue.push_back(left);
+                }
+
+                if let Some(right) = node.right.clone() {
+                    queue.push_back(right);
+                }
+            }
+        }
+
+        result.push(current_level);
+    }
+
+    result
+    }
+
+}
